376327f16e2deb40ad588ef9dc9d4dda054f28d8
weenfan <weenfan@gmail.com>
2016-09-15 20:25:30 -0700

    Update span.pm
    Fixed read-only error.

--- code/span.pm
+++ code/span.pm
@@ -7,21 +7,21 @@ sub quote
 	my $pos = \shift;
 
 	die if !defined $$pos;
-
+	my $npos = $$pos;
 	my $len = length($$in);
 	my $out = "";
 
 	while (1)
 		{
-		my $span = $len - $$pos;
+		my $span = $len - $npos;
 		$span = 0 if $span < 0;
 		$span = 255 if $span > 255;
 
 		$out .= chr($span);
 		return $out if $span <= 0;
 
-		$out .= substr($$in, $$pos, $span);
-		$$pos += $span;
+		$out .= substr($$in, $npos, $span);
+		$npos += $span;
 		}
 	}
 
@@ -31,17 +31,17 @@ sub unquote
 	my $pos = \shift;
 
 	die if !defined $$pos;
-
+	my $npos = $$pos;
 	my $len = length($$in);
 	my $out = "";
 
 	while (1)
 		{
 		last if $$pos >= $len;  # should not happen if well-formed
-		my $span = ord(substr($$in,$$pos,1));
-		$$pos += ($span + 1);
+		my $span = ord(substr($$in,$npos,1));
+		$npos += ($span + 1);
 		last if $span == 0;
-		$out .= substr($$in, $$pos - $span, $span);
+		$out .= substr($$in, $npos - $span, $span);
 		}
 
 	return $out;
